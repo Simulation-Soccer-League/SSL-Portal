4d8766cd9b679fe0af4314104e0b67a4e34885ce
Kenny Gilles <kenny.gilles@gmail.com>
2025-03-23 17:06:10 -0400

    Copy tags changes over from other repo

--- app/logic/ui/tags.R
+++ app/logic/ui/tags.R
@@ -4,30 +4,44 @@ box::use(
 
 
 #' @export
-flexCol <- function(cont, style = "") {
-  shiny::tag("div", varArgs = list(cont, class = "flex-col-wrapper", style = style))
+flexCol <- function(cont, style = "", onclick = "") {
+  shiny::tag("div", varArgs = list(cont, class = "flex-col-wrapper", style = style, onclick = onclick))
 }
 
 #' @export
-flexRow <- function(cont, style = "") {
-  shiny::tag("div", varArgs = list(cont, class = "flex-row-wrapper", style = style))
+flexRow <- function(cont, style = "", onclick = "") {
+  shiny::tag("div", varArgs = list(cont, class = "flex-row-wrapper", style = style, onclick = onclick))
 }
 
 #' @export
-navMenu <- function(cont, label = "", items = list()) {
+navMenu <- function(cont, label = "", items = list(), showItems = FALSE) {
   if (!missing(cont) && label == "") {
     shiny::tag("div", varArgs = list(cont, class = "nav-menu"))
   } else if (length(label) > 0) {
-    # TODO: Height of each items div doesn't affect other nav menu items
+    itemsClassNames <- c("nav-menu_items", if (showItems) " show-items" else "")
+
     div(
       class = "nav-menu",
       role = "button",
       onclick = "
-        const menuItems = this.querySelector('.nav-menu_items');
-        const maxContent = 'max-content';
-        if (menuItems) {
-          const isOpen = getComputedStyle(menuItems).height === maxContent;
-          menuItems.style.height = isOpen ? '0px' : maxContent;
+        const allMenuItems = document.querySelectorAll('.nav-menu_items');
+        const allSubMenuItems = document.querySelectorAll('.nav-menu_sub-items');
+        const childMenuItems = this.querySelector('.nav-menu_items');
+        const childSubMenuItems = this.querySelector('.nav-menu_sub-items');
+
+        if (childMenuItems) {
+          const isClosed = getComputedStyle(childMenuItems).height === '0px';
+
+          // Close all other open menus
+          if (isClosed) {
+            [...allMenuItems, ...allSubMenuItems].forEach(item => {
+              if (item !== childMenuItems && item !== childSubMenuItems) {
+                item.style.height = '0px';
+              }
+            });
+          }
+
+          childMenuItems.style.height = 'max-content';
         }
       ",
       tagList(
@@ -40,13 +54,32 @@ navMenu <- function(cont, label = "", items = list()) {
         ),
         if (length(items) > 0) {
           div(
-            class = "nav-menu_items",
+            class = itemsClassNames,
             role = "button",
             flexCol(
               tagList(
                 tags_list <- lapply(items, function(item) {
                   div(
                     class = "nav-menu_item",
+                    onclick = "
+                      const allSubMenuItems = document.querySelectorAll('.nav-menu_sub-items');
+                      const childSubMenuItems = this.querySelector('.nav-menu_sub-items');
+
+                      if (childSubMenuItems) {
+                        const isClosed = getComputedStyle(childSubMenuItems).height === '0px';
+
+                        // Close all other open menus
+                        if (isClosed) {
+                          allSubMenuItems.forEach(item => {
+                            if (item !== childSubMenuItems) {
+                              item.style.height = '0px';
+                            }
+                          });
+                        }
+
+                        childSubMenuItems.style.height = 'max-content';
+                      }
+                    ",
                     item
                   )
                 })
@@ -64,10 +97,17 @@ navMenuItem <- function(cont, label = "", subItems = list()) {
   if (length(subItems) > 0) {
     tagList(
       flexRow(
-        style = "align-items: center; justify-content: space-between; gap: 4px; padding: 8px;",
+        style = "align-items: center; justify-content: space-between; gap: 4px;",
         tagList(
           span(label, role = "button"),
-          icon("caret-right")
+          div(
+            class = "nav-menu_item-caret-right",
+            icon("caret-right")
+          ),
+          div(
+            class = "nav-menu_item-caret-down",
+            icon("caret-down")
+          )
         )
       ),
       div(
