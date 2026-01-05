4dc0f432615cfe5ea9a4f934f40ddb588dbc9ab8
Canadice <33020567+canadice@users.noreply.github.com>
2026-01-05 17:22:27 +0100

    Adding grouped TPE reporting for reduction in message size

--- app/logic/db/discord.R
+++ app/logic/db/discord.R
@@ -1,6 +1,7 @@
 ## Function for queries to mybb
 box::use(
   dplyr,
+  purrr[map],
   shinyjs[runjs],
   stringr[str_remove_all],
 )
@@ -45,95 +46,101 @@ sendTest <- function(){
 #' @export
 sendGradedTPE <- function(data){
   
-  gradedString <- 
-    apply(
-      data |> 
-        dplyr$select(username, tpe, source), 
-      1, 
-      function(row) paste(row, collapse = " - ")
-    ) |> 
-    paste(collapse = "\\n")
+  map(data$source |> unique(),
+      .f = function(x){
+        gradedString <- 
+          apply(
+            data |> 
+              dplyr$filter(source == x) |> 
+              dplyr$select(username, tpe), 
+            1, 
+            function(row) paste(row, collapse = " - ")
+          ) |> 
+          paste(collapse = "\\n")
+        
+        if(nchar(gradedString) > 1024){
+          result <- 
+            apply(
+              data |> 
+                dplyr$filter(source == x) |> 
+                dplyr$select(username, tpe), 
+              1, 
+              function(row) paste(row, collapse = " - ")
+            )
+          
+          splits <- nchar(gradedString) %/% 1024 + 1
+          
+          group <- rep(1:splits, length.out = length(result))
+          
+          splitResult <- split(result, group)
+          
+          strings <- lapply(splitResult, function(x) x |> paste(collapse = "\\n")) |> 
+            unlist()
+          
+          jscode <- paste0("
+            function sendMessage() {
+              const request = new XMLHttpRequest();
+              request.open('POST', '", tpe, "');
+              request.setRequestHeader('Content-type', 'application/json');
+              var myEmbed = {
+                author: {name: 'A new PT has been graded!'},
+                title: '", x, "',
+                fields: [", 
+                                   paste0(
+                                     "{name: '', 
+                      value: '", sprintf("```%s```", strings), "'}",
+                                     collapse = ","
+                                   ),
+                                   "],
+                footer: {
+                  text: 'If you have received 0 or reduced TPE, please check a summary post in the PT thread. \\n\\nThe TPE has already been added to your player page, this is just a report.'
+                } 
+              };
+              
+              var params = {username: 'PT Watcher',embeds: [ myEmbed ]};
+              
+              request.send(JSON.stringify(params));
+            }
+            
+            sendMessage();"
+          )
+          
+          # cat(jscode)
+          
+          runjs(jscode)
+          
+        } else {
+          jscode <- paste0("
+            function sendMessage() {
+              const request = new XMLHttpRequest();
+              request.open('POST', '", tpe, "');
+              request.setRequestHeader('Content-type', 'application/json');
+              var myEmbed = {
+                author: {name: 'A new PT has been graded!'},
+                title: '", x, "',
+                fields: [ 
+                  {name: '', 
+                   value: '", sprintf("```%s```", gradedString), "'}
+                ],
+                footer: {
+                  text: 'If you have received 0 or reduced TPE, please check a summary post in the PT thread. \\n\\nThe TPE has already been added to your player page, this is just a report.'
+                } 
+              };
+              
+              var params = {username: 'PT Watcher',embeds: [ myEmbed ]};
+              
+              request.send(JSON.stringify(params));
+            }
+            
+            sendMessage();"
+          )
+          
+          # cat(jscode)
+          
+          runjs(jscode)
+        }
+      })
   
-  if(nchar(gradedString) > 1024){
-    result <- 
-      apply(
-        data |> 
-          dplyr$select(username, tpe, source), 
-        1, 
-        function(row) paste(row, collapse = " - ")
-      )
-    
-    splits <- nchar(gradedString) %/% 1024 + 1
-    
-    group <- rep(1:splits, length.out = length(result))
-    
-    splitResult <- split(result, group)
-    
-    strings <- lapply(splitResult, function(x) x |> paste(collapse = "\\n")) |> 
-      unlist()
-    
-    jscode <- paste0("
-    function sendMessage() {
-      const request = new XMLHttpRequest();
-      request.open('POST', '", tpe, "');
-      request.setRequestHeader('Content-type', 'application/json');
-      var myEmbed = {
-        author: {name: 'A new PT has been graded!'},
-        title: 'User - TPE - Source',
-        fields: [", 
-                     paste0(
-                       "{name: '', 
-              value: '", sprintf("```%s```", strings), "'}",
-                       collapse = ","
-                     ),
-                     "],
-        footer: {
-          text: 'If you have received 0 or reduced TPE, please check a summary post in the PT thread. \\n\\nThe TPE has already been added to your player page, this is just a report.'
-        } 
-      };
-      
-      var params = {username: 'PT Watcher',embeds: [ myEmbed ]};
-      
-      request.send(JSON.stringify(params));
-    }
-    
-    sendMessage();"
-    )
-    
-    # cat(jscode)
-    
-    runjs(jscode)
-    
-  } else {
-    jscode <- paste0("
-    function sendMessage() {
-      const request = new XMLHttpRequest();
-      request.open('POST', '", tpe, "');
-      request.setRequestHeader('Content-type', 'application/json');
-      var myEmbed = {
-        author: {name: 'A new PT has been graded!'},
-        title: 'User - TPE - Source',
-        fields: [ 
-          {name: '', 
-           value: '", sprintf("```%s```", gradedString), "'}
-        ],
-        footer: {
-          text: 'If you have received 0 or reduced TPE, please check a summary post in the PT thread. \\n\\nThe TPE has already been added to your player page, this is just a report.'
-        } 
-      };
-      
-      var params = {username: 'PT Watcher',embeds: [ myEmbed ]};
-      
-      request.send(JSON.stringify(params));
-    }
-    
-    sendMessage();"
-    )
-    
-    # cat(jscode)
-    
-    runjs(jscode)
-  }
 }
 
 #' @export
